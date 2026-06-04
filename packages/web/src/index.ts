b755660fb864eed803bf036315ad0acd1f5ea7e4
mithun <mithun@viya.ae>
2026-06-04 15:09:18 +0400

    index pages changes

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -2,6 +2,8 @@ import "./styles/theme.css";
 
 import { registerCustomElements } from "./core/register";
 import { MButton } from "./components/button";
+import { MIcon } from "./components/icon";
+import { MText, MDisplay, MHeading, MTitle, MBody, MCaption } from "./components/text";
 
 export { MButton } from "./components/button";
 export type {
@@ -10,6 +12,26 @@ export type {
   MButtonType,
   MButtonVariant
 } from "./components/button";
+
+export { MIcon } from "./components/icon";
+export type { MIconColor, MIconSize } from "./core/icons";
+
+export { MText, MDisplay, MHeading, MTitle, MBody, MCaption } from "./components/text";
+export type {
+  MTextAlign,
+  MTextAnimation,
+  MTextAs,
+  MTextColor,
+  MTextFamily,
+  MTextFormat,
+  MTextGradient,
+  MTextLeading,
+  MTextSize,
+  MTextTracking,
+  MTextVariant,
+  MTextWeight
+} from "./core/typography";
+
 export { MithraElement } from "./core/mithra-element";
 export { applyTheme } from "./core/theme";
 export type { MithraDirection, MithraTheme, ThemeOptions } from "./core/theme";
@@ -21,6 +43,13 @@ export type {
 
 export function register(): void {
   registerCustomElements({
-    "m-button": MButton
+    "m-button": MButton,
+    "m-icon": MIcon,
+    "m-text": MText,
+    "m-display": MDisplay,
+    "m-heading": MHeading,
+    "m-title": MTitle,
+    "m-body": MBody,
+    "m-caption": MCaption
   });
 }
